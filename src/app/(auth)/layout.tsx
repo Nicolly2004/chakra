012d59b52515024b00f1ecd041c9ded530886419
Nicolly Reis <nicollyro2004@gmail.com>
2023-04-25 22:01:22 -0300

    Cria a estrutura inicial das telas de autenticação

---
+++ src/app/(auth)/layout.tsx
@@ -0,0 +1,19 @@
+'use client'
+import { Flex } from '@chakra-ui/react'
+import { ReactNode } from 'react'
+
+interface AutLayoutProps{
+    children: ReactNode
+}
+
+export default function AuthLayout({ children }: AutLayoutProps) {
+   return <Flex
+    bg="gray.50"
+     minH="100vh"
+     minW="100vw"
+     align="center"
+     justify="center"
+     >
+        {children}
+     </Flex>
+}
